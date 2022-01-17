f6812de619758d07f8fe1b2031f81446c1587437
Kaike Yudi <kaikeyudi@outlook.com>
2022-01-17 15:45:15 -0300

    Requisito 3 - OK

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (typeof name !== 'string' || typeof age !== 'number') {
+    return undefined;
+  }
+  const string = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return string;
+};
 
 module.exports = vqv;
