9e70f8c4dc74e24cf8c55f0c8f573cd0877f161d
Kaike Yudi <kaikeyudi@outlook.com>
2022-01-17 15:31:49 -0300

    Requisito 1 - OK

--- src/average.js
+++ src/average.js
@@ -11,6 +11,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+  }
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
+  }
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
